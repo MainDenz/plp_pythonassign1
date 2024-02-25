0ef1016c01cf255a89e85ac4ff61ab7b3d694f39
Denzel Njuki <denzelnjuki@Denzels-MacBook-Pro.local>
2024-02-25 03:51:38 +0300

    First Commit

---
+++ user_input.py
@@ -0,0 +1,9 @@
+ 
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+
+
+print("Thank you, "+ name + ". Your age is " + age + " and you live in "+ location)
+
